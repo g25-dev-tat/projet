0da28362f1b6bc9d5e21c2e85b0f9b44e5af53ba
Samson NZUMGUENG <nzumgues@3il.fr>
2020-06-12 00:17:08 +0200

    Ajout d'un jeu de données (bénévoles et participants)

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -49,12 +49,35 @@ VALUES (1,'Samson','NZUMGUENG',0642279765,'nzumgues@3il.fr','9 rue deverrine, 87
 (3,'Fabrice','NZUMGUENG',0642279765,'nzumguef@3il.fr','9 rue deverrine, 87000, limoges','recuperer dossards/puces, moto (ouverture, fermeture), photographe','12-12-2020',true),
 (4,'Frances','NZUMGUENG',0642279765,'nzumguefr@3il.fr','9 rue deverrine, 87000, limoges','buvette, repas, photographe','12-12-2020',true),
 (5,'Joel','NZUMGUENG',0642279765,'nzumguej@3il.fr','9 rue deverrine, 87000, limoges','ravitaillement, secu eau, remise dossards','12-12-2020',true),
-(6,'Danielle','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true);
+(7,'Danielle','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, bordeaux','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(8,'Jordan','TCHEA',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true), 
+(9,'Bill','TSOFACK',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(10,'Chloe','BACKERT',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 97080, paris','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(11,'Phoebe','THUNDERMAN',0642279765,'nzumgueda@3il.fr','19 rue jean la-mace, 87110, lyon','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(12,'Max','THUNDERMAN',0642279765,'nzumgueda@3il.fr','1 rue Marshall, 77000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(13,'Billy','THUNDERMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(14,'Nora','THUNDERMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(15,'Thierry','THUNDERMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(16,'Chloe','THUNDERMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(17,'Barb','THUNDERMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(18,'Kenzie','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(19,'Babe','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(20,'Houston','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(21,'Triple-G','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(22,'Double-G','NZUMGUENG',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(23,'Kal','LIGHTMAN',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(24,'Penny','HOFSHTATER',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(25,'Howard','WOLOWITZ',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(26,'Raj','KOUTRAPALI',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(27,'Emmy','CORINE',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(28,'Amanda','TOCHE',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(29,'Amanda','CLARK',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true),
+(30,'Amy','COOPER',0642279765,'nzumgueda@3il.fr','9 rue deverrine, 87000, limoges','signaleur, ravi eau, Chrono, remise dossards','12-12-2020',true);
 
 
 --Equipe
 INSERT INTO "equipe"(Id, nomeq, paye, valide, commentaire, nbrerepas)
-VALUES (1, 'NZUMGUENG.TCHEA',true,true,'Ok',2),(2, 'TOCHE.KOWA',true,true,'Ok',4),(3, 'THIBAUT.KUATE',false,true,'Ok',5);
+VALUES (1, 'NZUMGUENG.TCHEA',true,true,'Ok',2),(2, 'TOCHE.KOWA',true,true,'Ok',4),(3, 'THIBAUT.KUATE',false,true,'Ok',5),(4, 'MAMNO.KENGNE',false,true,'Ok',4),(5, 'TAMOKOUE.DJUIKOM',false,true,'Ok',7);
 
 
 --Participant
@@ -64,5 +87,35 @@ VALUES (1, 'NZUMGUENG', 'Samson', 0642279765, 'nzumges@3il.fr', 'rue deverrine, 
 (3, 'TOCHE', 'Harry', 0642279765, 'harry@3il.fr', '01 rue gustave nadaud, 87000, limoges', 'justificatif-capitaine.pdf', 'Capitaine', '12-12-2020','3iL',2),
 (4, 'KOWA', 'Daniel', 0642279765, 'kowad@3il.fr', '12 places des faeinciers, 76100, Rouen', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',2),
 (5, 'THIBAUT', 'Ron', 0642279765, 'thibaut@3il.fr', '1 place Winston churchill, 87000, limoges', 'justificatif-capitaine.pdf', 'Capitaine', '12-12-2020','3iL',3),
-(6, 'KUATE', 'Fabrice', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3);
+(6, 'KUATE', 'Fabrice', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(7, 'MAMNO', 'Danielle', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-capitaine', 'Capitaine', '12-12-2020','3iL',4),
+(8, 'KENGNE', 'Frances', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-Capitaine.pdf', 'Capitaine', '12-12-2020','3iL',4),
+(9, 'TAMOKOUE', 'Joel', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',5),
+(10, 'DJUIKOM', 'Manuela', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',5),
+(11, 'DJUKOM', 'Hilary', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(12, 'KAMNO', 'Lys', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(13, 'KAMDEM', 'Joel', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(14, 'HOFSHTATER','Leonard', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(15, 'COOPER', 'Sheldon', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(16, 'FARHA-FOLER', 'Emmy', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(17, 'SANTIAGO', 'Amy', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(18, 'PERALTHA', 'Jake', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(19, 'LIMOY', 'Arthur', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(20, 'LIGHTMAN', 'Emily', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(21, 'TORRES', 'Ria', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(22, 'MORNING-STAR', 'Lucifer', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(23, 'GRANGER', 'Hermione', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(24, 'WISLEY', 'Ronald', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(25, 'POTTER', 'Harry', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(26, 'WISLEY', 'Jenny', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(27, 'MALFOY', 'Drago', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(28, 'BELLE-DAME', 'Manon', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(29, 'DUBOIS', 'Claire-Helene', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(30, 'ASSOUKE', 'Sophia', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(31, 'TSOFACK', 'Bill', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(32, 'RETORY', 'Amanda', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(33, 'KAMENI', 'Donald', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(34, 'TONLEU', 'Gislain', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(35, 'WOFA', 'Paolyne', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3),
+(36, 'MANKU', 'Heidi', 0642279765, 'kuatef@3il.fr', '1 boulevard e la republique, 91000, Paris', 'justificatif-equipier.pdf', 'Equipier', '12-12-2020','3iL',3);
 
